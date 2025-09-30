a3928026d73f4a9815c68df0c4fdac5a4c4f0cb3
c0a24127ee <c0a24127ee@edu.teu.ac.jp>
2025-09-30 14:11:24 +0900

    爆弾を作った

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP: (0, -5),
-    pg.K_DOWN: (o, +5),
+    pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
@@ -16,10 +17,20 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20,20))  # 空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅の黒いのが消える
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # 爆弾座標
+    vx, vy = +5, +5  # 爆弾の速度
+
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -32,8 +43,8 @@ def main():
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]#横方向
-                sum_mv[1] += mv[1]#縦方向
+                sum_mv[0] += mv[0]  # 横方向
+                sum_mv[1] += mv[1]  # 縦方向
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -45,6 +56,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
