dfc85a96cc976a3873579ebd48aeb00b44f13263
c0a24127ee <c0a24127ee@edu.teu.ac.jp>
2025-09-30 17:20:39 +0900

    爆弾がでかくなる

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,6 +48,19 @@ def gameover(screen:pg.Surface) -> None:  # ゲームオーバー画面
     screen.blit(bo_img, (0, 0))
     pg.display.update()
     time.sleep(5)
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = []
+
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+    bb_accs = [a for a in range(1, 11)] # 加速度のリスト
+    
+    return bb_imgs, bb_accs
+
     
 
 
@@ -66,11 +79,20 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # 爆弾座標
     vx, vy = +5, +5  # 爆弾の速度
+    
 
     
     clock = pg.time.Clock()
     tmr = 0
+    bb_imgs, bb_accs = init_bb_imgs()
+    avx = vx * bb_accs[min(tmr//500, 9)]
+    bb_img = bb_imgs[min(tmr//500, 9)]
+    
+    
     while True:
+        bb_imgs, bb_accs = init_bb_imgs()
+        avx = vx * bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
@@ -99,6 +121,12 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
+
+
+        
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
         bb_rct.move_ip(vx,vy)
         yoko,tate = check_bound(bb_rct)
         if not yoko:  # 横方向にはみ出ていたら
